c15b1839127da0930528b5c7fc42f463389dcd5d
Goomerr <kemedices@gmail.com>
2022-08-01 17:26:41 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '03bdcf7f457278e604701962372c8273'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2243be33-4284-4719-be64-09954a70699f'
+};
